c36a797ac19dc32e71eac4bc45872a2ce243a7d0
Lalka00pq <lalka00pq@gmail.com>
2025-02-10 13:20:10 +0300

    Refactoring code

--- src/1_task_entrance_task/main.py
+++ src/1_task_entrance_task/main.py
@@ -1,33 +1,30 @@
 def caesar_method(alphabet: list, shift: int, message: str) -> str:
     ''' Функция шифрования сообщения шифром Цезаря
-    :param alphabet:
-    :param shift:
-    :param message:
-    :return: new_message
+    :param alphabet (list): Алфавит для шифрования
+    :param shift (int): Сдвиг для символов в алфавите
+    :param message (str): Сообщение для шифрования(расшифрования)
+    :return: new_message (str): Зашифрованное(Расшифрованное) сообщение
     '''
     new_message = ""
-    if not message:
-        return 'String is empty'
-    else:
-        for i in message:
-            try:
-                new_index = (alphabet.index(i) + shift) % len(alphabet)
-                new_message += alphabet[new_index]
-            except ValueError:
-                new_message = 'Error'
-                break
-        return new_message
+    for i in message:
+        try:
+            new_index = (alphabet.index(i) + shift) % len(alphabet)
+            new_message += alphabet[new_index]
+        except ValueError:
+            new_message = 'Error'
+            break
+    return new_message
 
 
-def main():
+def main() -> None:
     '''
-    Главная функция
-    :return:
+    Главная функция, вызывающая функцию шифрования
+    :return: None
     '''
     alphabet = list(' abcdefghijklmnopqrstuvwxyz')
     shift = int(input("Enter shift number (only integer): "))
     message = str(input("Enter message: ")).strip()
-    result = caesar_method(alphabet,shift, message)
+    result = caesar_method(alphabet, shift, message)
     print(result)
 
 
